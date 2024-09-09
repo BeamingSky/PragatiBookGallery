29bca80c4a35b8ea429cabbbce7f3e90e5f935d7
Akash Sharma <akash.ibm121@gmail.com>
2024-09-09 08:11:58 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Open the image popup
+document.getElementById('productImage').addEventListener('click', function() {
+  var popupContainer = document.getElementById('imagePopupContainer');
+  var popupImage = document.getElementById('popupImage');
+  
+  // Set the image source to the clicked image's source
+  popupImage.src = this.src;
+
+  // Show the popup
+  popupContainer.style.display = 'flex';
+});
+
+// Close the popup when clicking the close button
+document.getElementById('closePopup').addEventListener('click', function() {
+  document.getElementById('imagePopupContainer').style.display = 'none';
+});
+
+
+// Close the popup when clicking the close button
+document.getElementById("closePopup").addEventListener("click", function () {
+  document.getElementById("imagePopupContainer").style.display = "none";
+});
+
+// Close the popup when clicking outside of the image
+document
+  .getElementById("imagePopupContainer")
+  .addEventListener("click", function (event) {
+    var popupImage = document.getElementById("popupImage");
+
+    // Check if the click was outside of the image
+    if (event.target !== popupImage) {
+      document.getElementById("imagePopupContainer").style.display = "none";
+    }
+  });
+
+// Close the popup when pressing the "Esc" key
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    document.getElementById("imagePopupContainer").style.display = "none";
+  }
+});
